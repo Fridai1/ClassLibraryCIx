9ced1918e8f1ca403a030129cf876197a0df526c
Fridai <Nikolaiviby3@gmail.com>
2018-03-05 11:37:52 +0100

    med unittest

---
+++ ClassLibraryCI/ClassLibraryCI/Student.cs
@@ -0,0 +1,7 @@
+﻿namespace ClassLibraryCI
+{
+    public class Student
+    {
+        
+    }
+}
---
+++ ClassLibraryCI/ClassLibraryCITests/Class1Tests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryCI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCI.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void GetNameTest()
+        {
+            Class1 s = new Class1();
+            Assert.AreEqual("Nikolai", s.GetName());
+        }
+
+        [TestMethod()]
+        public void GetAdressTest()
+        {
+            Class1 s = new Class1();
+            Assert.AreEqual("elisagaardsvej 5", s.GetAdress());
+        }
+    }
+}
